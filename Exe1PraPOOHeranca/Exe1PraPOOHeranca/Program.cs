a9db53d45664a10d9190e22d5e986764ae410806
diotec <claudiojoc46@mail.ru>
2017-09-29 11:21:31 +0300

    Heranca entre as classes Circulo e Centro

---
+++ Exe1PraPOOHeranca/Exe1PraPOOHeranca/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Exe1PraPOOHeranca
+{
+    public class Centro
+    {
+        private int x, y;
+        public Centro ()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Centro (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void CoordCentro()
+        {
+            Console.WriteLine("Centro = ( {0}, {1} )", x, y);
+        }
+    }
+    public class Circulo : Centro
+    {
+        private double Raio;
+        public Circulo ()
+        {
+
+        }
+        public Circulo(int x,int y, double R): base (x,y)
+        {
+            Raio = R;
+        }
+        public double Area()
+        {
+            return Math.Round(Math.PI * Math.Pow(Raio, 2), 2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 4;
+            Circulo[] C = new Circulo[N];
+            int X = 0, Y = 0;
+            double R = 0.5;
+            for (int I =0; I<C.Length;I++)
+            {
+                C[I] = new Circulo(X, Y, R);
+                C[I].CoordCentro();
+                Console.WriteLine("Area = {0}", C[I].Area());
+                X++;
+                Y++;
+                R *= 2;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
